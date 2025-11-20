86c5494c1ee7f0868148a1afadaa14edabd09cfa
Jack Wen <jackwen04@gmail.com>
2025-11-20 17:13:21 +0800

    controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,7 +1,29 @@
 import asyncio
+
 from queue_manager import command_queue, event_queue
+from importlib import import_module
+from config import *
+from common import *
+import os
 
 
 class Controller:
     def __init__(self):
-        pass
+        self._state = ""
+        self._tasks = []
+
+
+    def _load_tasks(self):
+        self._modules = []
+        for task in TASKS:
+            path = "controller.tasks." + task["module"]
+            self._modules.append(import_module(path))
+
+    async def loop(self):
+        while True:
+            command = await command_queue.get()
+
+
+if __name__ == "__main__":
+    controller = Controller()
+    controller._load_tasks()
